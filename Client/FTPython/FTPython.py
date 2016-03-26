d4df96887ecf92b691ac5c6257595260547e28fe
MiRIr <MiRIr@users.noreply.github.com>
2016-03-26 03:40:19 -0500

    Rework
    This has so many changes it would take time to go over them all.
    
    In general, it has been reworked in order to function more nicely and
    errors are now isolated in threads and don't bleed throughout the
    program. Like other FTP servers, there are now separate data channels,
    which helps separate requests and reply codes.

--- Client/FTPython/FTPython.py
+++ Client/FTPython/FTPython.py
@@ -2,89 +2,131 @@
 import getpass
 import socket
 import time
+import thread
 
-def trySend(s, i):
-    try:
-        if len(i) > 0:
-            s.send(i)
-    except:
-        print 'Server Disconnected'
+dataChannels = []
+binary = True
 
-def trySendSecure(s, i):
-    print 'Not Implemented'
+def Send(s, i):
+	try:
+		s.send(i + '\n')
+	except:
+		print 'The server is no longer connected.'
+
+def ServerOutput(s):
+	while True:
+		try:
+			out = str(s.recv(4096))
+			print out
+			if out.startswith('227'):
+				response = out.split('(')[1].split(',')
+				ip = response[0] + '.' + response[1] + '.' + response[2] + '.' + response[3]
+				port = int(response[4]) * 256 + int(response[5].replace(')', ''))
+				global dataChannels
+				dataChannels[0] = (dataChannels[0],) + (ip, port)
+		except:
+			break
+
+def HandleDataChannels():
+	global dataChannels
+	while True:
+		if len(dataChannels) > 0:
+			if len(dataChannels[0]) == 3:
+				global binary
+				if (dataChannels[0][0].upper().startswith('RETR') or dataChannels[0][0].upper().startswith('GET')) and binary:
+					thread.start_new_thread(DownloadFile, (dataChannels[0],))
+					dataChannels.pop(0)
+				elif dataChannels[0][0].upper().startswith('LIST') or dataChannels[0][0].upper().startswith('MLSD') or not binary:
+					thread.start_new_thread(HandleASCII, (dataChannels[0],))
+					dataChannels.pop(0)
+			
+def DownloadFile(info):
+	try:
+		c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		c.connect((info[1], info[2]))
+
+		f = open(info[0].split(' ', 1)[1], 'wb')
+		while True:
+			data = c.recv(4096)
+			if not data:
+				break
+			f.write(data)
+		f.close()
+		print 'Finished ' + info[0].split(' ', 1)[1]
+	except:
+		pass
+
+def HandleASCII(info):
+	try:
+		c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		c.connect((info[1], info[2]))
+
+		listing = ''
+		while True:
+			data = c.recv(16)
+			if not data:
+				break
+			listing = listing + data
+		print listing
+	except:
+		pass
 
 def main():
-    #ip = raw_input('Server Address- ')
+	#ip = raw_input('Server Address- ')
 
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            #s.connect(('10.20.122.203', 85))
-            s.connect((socket.gethostbyname('MichaelSurface'), 60383))
-            i = s.recv(1024)
-            print i
-            break
-        except:
-            print 'Server Not Found'
-            time.sleep(2)
+	while True:
+		while True:
+			try:
+				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				s.connect(('10.0.0.2', 831))
+				i = s.recv(1024)
+				print i
+				break
+			except:
+				print 'Server Not Found'
+				time.sleep(1)
 
-    while i.startswith('230') == False:
-        while i.startswith('331') == False:
-            i = raw_input('Username- ')
-            trySend(s, 'USER ' + i)
-            i = s.recv(1024)
-        print i
-        i = getpass.getpass('Password- ')
-        trySend(s, 'PASS ' + i)
-        i = s.recv(1024)
-        print i
-
-    while True:        
-        i = raw_input('> ')
-        while len(i) == 0:
-            i = raw_input('> ')
-        trySend(s, i)
-        cM = i.split(' ', 1)
-        if cM[0].upper() == 'GET' or cM[0].upper() == 'MGET':
-            fL = cM[1].split('|')
-            for fN in fL:
-                ret = s.recv(4096).split(' ')
-                if ret[0] =='Found':
-                    f = open(fN,'wb')
-                    print 'Filesize- ' + ret[1] + ' bytes'
-                    
-                    dl = 0
-                    while dl < int(ret[1]):
-                        print str(dl/int(ret[1])) + '%'
-                        f.write(s.recv(4096))
-                        dl = dl + 4096
-                    f.close()
-                    print '100%'
-        elif cM[0].upper() == 'QUIT':
-            print 'Disconnected from Server'
-            break
-        else:
-            print s.recv(4096)
-    s.close()
+		Send(s, 'USER LANLauncher')
+		Send(s, 'PASS')
+	
+		thread.start_new_thread(ServerOutput, (s,))
+		thread.start_new_thread(HandleDataChannels, ())
+		while True:
+			i = raw_input()
+			while len(i) == 0:
+				i = raw_input()
+			if i.upper() == 'QUIT':
+				break
+			if i.upper().startswith('RETR ') or i.upper().startswith('GET ') or i.upper().startswith('MLSD ') or i.upper().startswith('LIST '):
+				Send(s, 'PASV')
+				Send(s, i)
+				global dataChannels
+				dataChannels.insert(0, i)
+			elif i.upper().startswith('ASCII'):
+				global binary
+				binary = not binary
+			else:
+				Send(s, i)
+		s.close()
 
 ctypes.windll.kernel32.SetConsoleTitleA("FTPython")
 print 'Welcome to FTPython\n'
 
 main()
 
-# CD            <dir>
-# LS            -
-# DIR           -
-# *GET           <file>
-# PUT           <file>
-# MGET          <folder> || *.* || *.<ext> || <name>.* || <name>|<name>
-# MPUT          <folder> || *.* || *.<ext> || <name>.* || <name>|<name>
-# ASCII-EC      Enable ASCII, disable binary
-# BINARY        Enable binary, disable ASCII
-# *QUIT         Disconnect from the server
-# *COMPRESS     Enable/disable compression
-# *ENCRYPT      Enable/disable encryption
-# *NORMAL       Turn off encryption and compression
+# CD	CWD		<dir>
+# LS	LIST	-
+# DIR	MLSD	-
+# GET	RETR	<file>
+# PUT			<file>
+# MGET			<folder> || *.* || *.<ext> || <name>.* || <name>|<name>
+# MPUT			<folder> || *.* || *.<ext> || <name>.* || <name>|<name>
+# ASCII-EC		Enable ASCII, disable binary
+# BINARY		Enable binary, disable ASCII
+# QUIT			Disconnect from the server
+# COMPRESS		Enable/disable compression
+# ENCRYPT		Enable/disable encryption
+# NORMAL		Turn off encryption and compression
 # 
 # CRC for each packet
 # Asynchronously send packets
--- Server/FTPython Server/FTPython_Server.py
+++ Server/FTPython Server/FTPython_Server.py
@@ -1,84 +1,81 @@
-import ctypes
+﻿import ctypes
 import socket
 import os
 import thread
-
-wd = "C:\\FTPFiles\\"
-
-def GET(client, fN):
-    fN = wd + fN
-    if os.path.isfile(fN):
-        size = os.path.getsize(fN)
-        client.send('Found ' + str(size))
-                   
-        f = open(fN, 'rb')
-        l = f.read(1024)
-        while l:
-            print '1'
-            client.send(l)
-            print '2'
-            l = f.read(1024)
-            print '3'
-        f.close()
-        print '4'
-    else:
-        client.send('Not Found')
-
-def MGET(client, fL):
-    fL = fL.split('|')
-    for fN in fL:
-        GET(client, fN)
-        print 'YYY'
+import random
 
 def clientThread(client, ip):
-    loggedin = encrypt = compress = False
-    client.send('220 FTPython')
-    while True:
-        r = client.recv(1024).strip().split(' ', 1)
-        r[0] = r[0].upper()
-        print ip[0] + '>',
-        for i in r:
-            print i,
-        print ''
-        if r[0] == 'QUIT':
-            break
-        elif loggedin:
-            if r[0] == 'GET':
-                GET(client, r[1])
-            elif r[0] == 'MGET':
-                MGET(client, r[1])
-            elif r[0] == 'COMPRESS':
-                compress = not compress
-                client.send('Compression is now ' + ('enabled' if compress else 'disabled') + '.')
-            elif r[0] == 'ENCRYPT':
-                encrypt = not encrypt
-                client.send('Encryption is now ' + ('enabled' if encrypt else 'disabled') + '.')
-            elif r[0] == 'NORMAL':
-                compress = encrypt = False
-                client.send('Compression and encryption are now disabled.')
-            else:
-                client.send('202 Command Not Implemented')
-        elif r[0] == 'USER':
-            #Check User
-            client.send('331 Need Password')
-        elif r[0] == 'PASS':
-            #Check Password
-            client.send('230 Logged In')
-            #client.send('430 Incorrect Login')
-            loggedin = True
-        else:
-            client.send('202 Command Not Implemented')
-    client.close()
+	wd = 'F:\\Games\\LANLauncher\\Server\\Data\\'
+	loggedin = encrypt = compress = False
+	client.send('220 FTPython')
+
+	def SendFile(info): #port, fileName
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		print 'Opening Data Channel on Port ' + str(info[0])
+		s.bind((socket.gethostname(), info[0]))
+		s.listen(1)
+		channel, r = s.accept()
+		while True:
+			if ip[0] == r[0]:
+				print 'Client Connected to Data Channel'
+				break
+			else:
+				channel.close()
+				channel, r = s.accept()
+			break
+
+		fN = wd + info[1].strip()
+		if os.path.isfile(fN):
+			f = open(fN, 'rb')
+			l = f.read(4096)
+			while l:
+				channel.send(l)
+				l = f.read(4096)
+			f.close()
+			print 'Client has finished downloading ' + info[1]
+			client.send('226 Successfully transferred ' + info[1])
+		else:
+			print info[1] + ' was not found'
+			client.send('550 File not found')
+		channel.close()
+
+	dataChannel = None
+	while True:
+		try:
+			out = str(client.recv(4096))
+			print out
+			if out.upper().startswith('QUIT'):
+				break
+			elif out.upper().startswith('PASV'):
+				port = random.randint(32768, 65535)
+				client.send('227 Entering Passive Mode (' + ip[0].replace('.', ',') + ',' + str(port / 256) + ',' + str(port % 256) + ')')
+				dataChannel = port
+			elif out.upper().startswith('RETR') or out.upper().startswith('GET'):
+				response = out.split(' ', 1)
+				if dataChannel:
+					dataChannel = (dataChannel, response[1])
+					thread.start_new_thread(SendFile, (dataChannel,))
+					dataChannel = None
+				else:
+					client.send('500 Use the PASV command to create a data channel.')
+				#thread to handle data channel
+				#+Command
+			#elif handleLIST
+		except:
+			break
+
+	print ip[0] + ' has disconnected.'
+	client.close()
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((socket.gethostname(), 60383))
-    s.listen(20)
-    while True:
-        print socket.gethostname()
-        client, r = s.accept()
-        thread.start_new_thread(clientThread, (client, r))
-        print 'Found', r[0]
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((socket.gethostname(), 831))
+	s.listen(20)
+	while True:
+		print socket.gethostname()
+		client, r = s.accept()
+		thread.start_new_thread(clientThread, (client, r))
+		print 'Found ' + r[0]
 
 ctypes.windll.kernel32.SetConsoleTitleA("FTPython Server")
 print 'FTPython Server\n'
